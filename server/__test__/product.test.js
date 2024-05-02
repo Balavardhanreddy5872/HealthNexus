04f4bd6ce6331fae8b404537f70583695e08f897
PRAJWAL NAIK <123065690+Prajwal40@users.noreply.github.com>
2024-05-02 15:18:29 +0530

    added test

--- server/__test__/product.test.js
+++ server/__test__/product.test.js
@@ -3,7 +3,7 @@ const request = require("supertest")
 const baseURL = "http://localhost:8081/";
 
 describe('GET /api/product/get-medicine/:slug', () => {
-    it('should return 200 OK and a token if login is successful', async () => {
+    it('should return 200 OK and a product', async () => {
         const response = await request(baseURL)
             .get('api/product/get-medicine/Acamprol')
         expect(response.status).toBe(200);
